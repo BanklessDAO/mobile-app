288e50b020f74b783f1af979972141a5739900e8
Vlad Averin <vlad@orbitum.co>
2021-10-01 08:05:55 -0600

    Define the cross-app appearance constants

---
+++ Bankless/Presentation/Configuration/Appearance.swift
@@ -0,0 +1,52 @@
+//
+//  Created with ♥ by BanklessDAO contributors on 2021-09-30.
+//  Copyright (C) 2021 BanklessDAO.
+
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see https://www.gnu.org/licenses/.
+//
+    
+
+import Foundation
+import UIKit
+
+enum Appearance {
+    static let contentInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    static let contentPaddings = UIEdgeInsets(top: 8, left: 10, bottom: 8, right: 10)
+    
+    enum Text {
+        static let font: UIFont = .systemFont(ofSize: UIFont.systemFontSize)
+        
+        enum Heading {
+            enum H1 {
+                static let font: UIFont = .boldSystemFont(ofSize: UIFont.systemFontSize + 10.0)
+                static let height: CGFloat = 30.0
+            }
+            
+            enum H2 {
+                static let font: UIFont = UIFont.boldSystemFont(ofSize: UIFont.systemFontSize + 5)
+                static let height: CGFloat = 25.0
+            }
+        }
+    }
+    
+    enum Date {
+        static func localDateStringFormat(from date: Foundation.Date) -> String {
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = .current
+            dateFormatter.dateStyle = .short
+            
+            return dateFormatter.string(from: date)
+        }
+    }
+}
