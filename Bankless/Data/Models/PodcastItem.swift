edd76836d34fa0c44630849af8fcb24e92c9ea70
0xNSHuman <0xnshuman@protonmail.com>
2021-11-18 00:02:15 -0700

    Create models for Newsletter and Podcast data

---
+++ Bankless/Data/Models/PodcastItem.swift
@@ -0,0 +1,29 @@
+//
+//  Created with ♥ by BanklessDAO contributors on 2021-11-16.
+//  Copyright (C) 2021 BanklessDAO.
+
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see https://www.gnu.org/licenses/.
+//
+    
+
+import Foundation
+
+struct PodcastItem: Codable {
+    let id: String
+    let title: String
+    let description: String
+    let publishedAt: Date
+    let thumbnailURL: URL
+    let videoURL: URL
+}
