1e2116c07c31b8fe18a1ce96eb13b54cc7ee5529
0xNSHuman <0xnshuman@protonmail.com>
2021-11-18 00:01:05 -0700

    Define News service

--- Bankless/Services/News/NewsItemsResponse.swift
+++ Bankless/Services/News/NewsItemsResponse.swift
@@ -1,5 +1,5 @@
 //
-//  Created with ♥ by BanklessDAO contributors on 2021-09-30.
+//  Created with ♥ by BanklessDAO contributors on 2021-11-16.
 //  Copyright (C) 2021 BanklessDAO.
 
 //  This program is free software: you can redistribute it and/or modify
@@ -19,30 +19,15 @@
 
 import Foundation
 
-protocol AuthServiceDependency {
-    var authService: AuthService! { get set }
-}
-
-protocol IdentityServiceDependency {
-    var identityService: IdentityService! { get set }
-}
-
-protocol BanklessServiceDependency {
-    var banklessService: BanklessService! { get set }
-}
-
-protocol AchievementsServiceDependency {
-    var achievementsService: AchievementsService! { get set }
-}
-
-protocol TimelineServiceDependency {
-    var timelineService: TimelineService! { get set }
-}
-
-protocol NewsServiceDependency {
-    var newsService: NewsService! { get set }
-}
-
-protocol BountyBoardServiceDependency {
-    var bountyBoardService: BountyBoardService! { get set }
+struct NewsItemsResponse {
+    let newsletterItems: [NewsletterItem]
+    let podcastItems: [PodcastItem]
+    
+    internal init(
+        newsletterItems: [NewsletterItem],
+        podcastItems: [PodcastItem]
+    ) {
+        self.newsletterItems = newsletterItems
+        self.podcastItems = podcastItems
+    }
 }
--- Bankless/Services/News/NewsService.swift
+++ Bankless/Services/News/NewsService.swift
@@ -1,5 +1,5 @@
 //
-//  Created with ♥ by BanklessDAO contributors on 2021-09-30.
+//  Created with ♥ by BanklessDAO contributors on 2021-11-16.
 //  Copyright (C) 2021 BanklessDAO.
 
 //  This program is free software: you can redistribute it and/or modify
@@ -18,31 +18,8 @@
     
 
 import Foundation
+import RxSwift
 
-protocol AuthServiceDependency {
-    var authService: AuthService! { get set }
-}
-
-protocol IdentityServiceDependency {
-    var identityService: IdentityService! { get set }
-}
-
-protocol BanklessServiceDependency {
-    var banklessService: BanklessService! { get set }
-}
-
-protocol AchievementsServiceDependency {
-    var achievementsService: AchievementsService! { get set }
-}
-
-protocol TimelineServiceDependency {
-    var timelineService: TimelineService! { get set }
-}
-
-protocol NewsServiceDependency {
-    var newsService: NewsService! { get set }
-}
-
-protocol BountyBoardServiceDependency {
-    var bountyBoardService: BountyBoardService! { get set }
+protocol NewsService {
+    func listNewsItems() -> Observable<NewsItemsResponse>
 }
